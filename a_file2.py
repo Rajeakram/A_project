179acde4c1222118330a0674da978a7d6088d142
“Raje <shalehakramraje@yahoo.com>
2023-02-23 19:03:44 -0500

    Alom

--- a_file2.py
+++ a_file2.py
@@ -7,3 +7,19 @@ if "is"not in "a":
     print("yes it is")
 else:
     print("NO")
+
+x=5
+y=4
+if x < y:
+    print("yes i am big")
+else:
+    print("No i am small")
+a,b,c=   "Orange", "Banana", "Cherry"
+x= """My name is jaman,i come here in{},
+for study,i am{} years old< i was born in {}"""
+print(x.format(a,b,c))
+
+x = y =z ="Dhaka"
+
+a = "i love in {},i was born in {},i want to live in {}"
+print(a.format(x,y,z))
